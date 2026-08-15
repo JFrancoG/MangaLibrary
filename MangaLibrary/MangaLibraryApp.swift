bcc42819a973fac4533ffa56634029e1f82ad39c
Jesus Franco <drpuerk@gmail.com>
2026-08-15 13:11:08 +0200

    Initial Commit

---
+++ MangaLibrary/MangaLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaLibraryApp.swift
+//  MangaLibrary
+//
+//  Created by Jesús Franco on 15.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MangaLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
